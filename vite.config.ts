f408f35dc5a0f61d7b854db6c3e06c5c267b0214
cw <zhangxun_1@stu.scu.edu.cn>
2023-09-17 15:12:22 +0800

    结构完善

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,14 @@ import {ElectronBuildPlugin} from "./plugins/vite.electron.build";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-      vue(),
-      ElectronDevPlugin(),
-      ElectronBuildPlugin(),
+    vue(),
+    ElectronDevPlugin(),
+    ElectronBuildPlugin(),
   ],
     base: './', // 默认是绝对路径改为相对路径
     resolve: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
 })
